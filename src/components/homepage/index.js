261daf11e5778ef86e6b98e34d0311c7bb1bdf39
Cheri <cmerrym1@kent.edu>
2022-04-24 22:47:56 -0400

    homepage and nac components created

---
+++ src/components/homepage/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Homepage() {
+    return (
+        <section>
+            <h1 id="homepage">This is the homepage</h1>
+        </section>
+    );
+}
+
+export default Homepage;
---
+++ src/components/nav/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Nav() {
+    return (
+        <header>
+            <h2>
+                <a href="/">
+                    Schedule Builder
+                </a>
+            </h2>
+            <nav>
+                <a href="#homepage">
+                
+                </a>
+            </nav>
+        </header>
+    );
+}
+
+export default Nav;
